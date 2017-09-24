4e3efbb7248c6894ee642d3c54f2effcc3374501
Brian <brian.kitano@yale.edu>
2017-09-24 15:10:36 +0000

    parsing numbers v non numbers

--- pset2/NoAP.c
+++ pset2/NoAP.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
+bool is_all_digits(char *s);
 
 int main(int argc, char **argv) {
     
@@ -10,7 +12,13 @@ int main(int argc, char **argv) {
     
     int i;
     for (i = 1; i < argc; i++) {
-        printf("%s \n", argv[i]);
+        // printf("%s \n", argv[i]);
+        
+        if (is_all_digits(argv[i])) {
+            printf("Digits: %s \n", argv[i]);
+        } else {
+            printf("Non numbers: %s \n", argv[i]);
+        }
     }
     
     // the next arguments are the must-haves
@@ -19,4 +27,22 @@ int main(int argc, char **argv) {
     // the last arguments are the methods
     
     return 0;
+}
+
+// copied from class example arguments.c
+bool is_all_digits(char *s)
+{
+  while (isdigit(*s))
+    {
+      s++;
+    };
+
+  if (*s == '\0')
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
 }
