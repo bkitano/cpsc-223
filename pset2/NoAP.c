20315783994ab1b4cfc2b949d5faf372a917017e
Brian <brian.kitano@yale.edu>
2017-09-25 19:33:14 +0000

    a header file

--- pset2/NoAP.c
+++ pset2/NoAP.c
@@ -3,14 +3,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-bool is_all_digits(char *s);
-void swap(int *xp, int *yp);
-void bubble_sort(int nums[], int n);
-bool arithmetic(int nums[]);
-void greedy(int range, int must_includes[], int must_includes_length);
-void backward(int range, int must_includes[], int must_includes_length);
-void skip(int range, int must_includes[], int must_includes_length, int first, int step);
-bool is_in(int array[], int size, int number);
+#include "NoAP.h"
 
 int main(int argc, char **argv) {
     
